a43e1183280e605e9c4bf11b4aeb13c89f0a3f7b
Bardin08 <vbardin810@gmail.com>
2024-03-18 01:53:29 +0200

    Migrate LoremGen to net standard 2

--- LoremGen/LoremGen.cs
+++ LoremGen/LoremGen.cs
@@ -1,42 +1,52 @@
-﻿namespace Fusion.LoremGen;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-/// <summary>
-/// This is the Lorem Ipsum generator.
-/// To get the Lorem Ipsum as a string or byte sequence use on of the following methods:
-/// </summary>
-public static class LoremGen
+namespace Fusion.LoremGen
 {
-    private const int DefaultSeed = 42357;
-    private static Random _random = new(DefaultSeed);
-
-    public static IEnumerable<string> Words(int number, int seed = DefaultSeed)
+    /// <summary>
+    /// This is the Lorem Ipsum generator.
+    /// To get the Lorem Ipsum as a string or byte sequence use on of the following methods:
+    /// </summary>
+    public static class LoremGen
     {
-        SetGeneratorSeed(seed);
+        private const int DefaultSeed = 42357;
+        private static Random _random;
 
-        for (var i = 0; i < number; i++)
+        static LoremGen()
         {
-            var idx = _random.Next(0, DataSource.WordsNumber);
-            yield return DataSource.Words[idx];
+            _random = new Random(DefaultSeed);
         }
-    }
 
-    public static IEnumerable<string> Paragraphs(int number, int seed = DefaultSeed)
-    {
-        SetGeneratorSeed(seed);
-        var wordsInParagraph = _random.Next(60, 160);
-
-        for (var i = 0; i < number; i++)
+        public static IEnumerable<string> Words(int number, int seed = DefaultSeed)
         {
-            var indexes = Enumerable.Range(0, wordsInParagraph)
-                .Select(_ => _random.Next(0, DataSource.WordsNumber));
+            SetGeneratorSeed(seed);
 
-            var paragraph = string.Join(" ", indexes.Select(idx => DataSource.Words[idx]));
-            yield return paragraph;
+            for (var i = 0; i < number; i++)
+            {
+                var idx = _random.Next(0, DataSource.WordsNumber);
+                yield return DataSource.Words[idx];
+            }
         }
-    }
 
-    private static void SetGeneratorSeed(int seed)
-    {
-        _random = new Random(seed);
+        public static IEnumerable<string> Paragraphs(int number, int seed = DefaultSeed)
+        {
+            SetGeneratorSeed(seed);
+            var wordsInParagraph = _random.Next(60, 160);
+
+            for (var i = 0; i < number; i++)
+            {
+                var indexes = Enumerable.Range(0, wordsInParagraph)
+                    .Select(_ => _random.Next(0, DataSource.WordsNumber));
+
+                var paragraph = string.Join(" ", indexes.Select<int, string>(idx => DataSource.Words[idx]));
+                yield return paragraph;
+            }
+        }
+
+        private static void SetGeneratorSeed(int seed)
+        {
+            _random = new Random(seed);
+        }
     }
 }
